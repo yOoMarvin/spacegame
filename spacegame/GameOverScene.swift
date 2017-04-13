ff27905c60e331968ac9f3f96e7d9c208015ad32
yOoMarvin <marvin.messenzehl@gmail.com>
2017-04-13 09:49:49 +0200

    Added transition for game over scene

--- spacegame/GameOverScene.swift
+++ spacegame/GameOverScene.swift
@@ -27,5 +27,19 @@ class GameOverScene: SKScene {
     }
     
     
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        let touch = touches.first
+        if let location = touch?.location(in: self) {
+            let node = self.nodes(at: location)
+            
+            if node[0].name == "newGameButton" {
+                let transition = SKTransition.flipHorizontal(withDuration: 0.5)
+                let gameScene = GameScene(size: self.size)
+                self.view!.presentScene(gameScene, transition: transition)
+            }
+        }
+    }
 
 }
